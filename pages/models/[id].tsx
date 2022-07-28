ddaf2b3279231f236cc995909e3c9e4ceb1d0a6a
Brandon Fan <brandonfan1256@gmail.com>
2022-07-28 13:44:38 -0500

    update github source button

--- pages/models/[id].tsx
+++ pages/models/[id].tsx
@@ -75,27 +75,6 @@ const IndividualModelPage = ({
                             <Labels tasks={model.tasks} papers={model.papers}
                                     sensors={model.sensors} license={model.license} limit={100}/>
                         </div>
-                        <a
-                            href={model.github}
-                            target="_blank"
-                            className="inline-flex w-full sm:w-auto items-center px-4 py-3 mt-8 border border-transparent shadow-sm 
-                            text-base rounded-md text-white bg-gray-700 hover:bg-gray-800 
-                            focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500" rel="noreferrer"
-                        >
-                            <svg
-                                className="w-5 h-5 mr-5"
-                                aria-hidden="true"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
-                                    clipRule="evenodd"
-                                />
-                            </svg>
-                            View GitHub Source
-                        </a>
                     </div>
                     <div
                         className="flex items-center mt-5 lg:mt-0 flex-col space-y-2 w-full sm:w-auto lg:space-y-0 lg:flex-row lg:space-x-3">
@@ -116,6 +95,27 @@ const IndividualModelPage = ({
             </div>
             <div className="grid px-6 md:px-24 xl:px-0 grid-cols-1 lg:grid-cols-8 lg:divide-x">
                 <div className="lg:col-span-5 pb-16 pt-3 xl:px-24 lg:pr-12">
+                <a
+                            href={model.github}
+                            target="_blank"
+                            className="float-right mb-8 inline-flex w-full sm:w-auto items-center px-5 py-3 mt-6 border border-transparent shadow-sm 
+                            text-base rounded-md text-white bg-gray-700 hover:bg-gray-800 
+                            focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500" rel="noreferrer"
+                        >
+                            <svg
+                                className="w-5 h-5 mr-5"
+                                aria-hidden="true"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                            View GitHub Source
+                        </a>
                     <Markdown>
                         {markdown}
                     </Markdown>
